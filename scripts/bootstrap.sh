432e0767835e7f5d7fea81743d537ab48359590c
Jackson Delahunt <jackson@stemn.com>
2019-02-23 03:52:48 +1100

    jump

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -65,6 +65,11 @@ add-apt-repository ppa:kubuntu-ppa/backports && \
 # Install antigen
 curl -L git.io/antigen > /usr/local/bin/antigen.zsh
 
+# Install jump directory navigator
+RUN wget -nv -O jump.deb https://github.com/gsamokovarov/jump/releases/download/v0.22.0/jump_0.22.0_amd64.deb && \
+  dpkg -i jump.deb && \
+rm jump.deb
+
 # Make gosu accessible to all users
 chown :users /usr/sbin/gosu && \
   chmod +s /usr/sbin/gosu
