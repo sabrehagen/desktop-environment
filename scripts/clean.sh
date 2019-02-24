31de4e85f4ac2e55583b755d01047918cf26e4a1
Jackson Delahunt <jackson@stemn.com>
2019-02-24 16:41:17 +1100

    no-clean-secrets

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,6 +4,5 @@ REPO_ROOT=$(dirname $(realpath $0))/..
 $REPO_ROOT/scripts/stop.sh 2>/dev/null
 
 # Remove volumes that can be recreated from scratch, directly from the container filesystem
-docker volume rm DESKTOP_ENVIRONMENT_CACHE_SECRETS
 docker volume rm DESKTOP_ENVIRONMENT_CACHE_VCSH
 docker volume rm DESKTOP_ENVIRONMENT_USER_HOME
