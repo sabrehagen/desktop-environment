e659eacff4e64c52fbfd6805b290e9b3797bfc60
Jackson Delahunt <jackson@stemn.com>
2019-02-24 20:05:29 +1100

    ssh

--- docker/scripts/clean-secrets.sh
+++ docker/scripts/clean-secrets.sh
@@ -5,4 +5,5 @@ $REPO_ROOT/docker/scripts/clean.sh
 
 # Remove volumes containing secrets
 docker volume rm DESKTOP_ENVIRONMENT_CACHE_SECRETS
+docker volume rm DESKTOP_ENVIRONMENT_CACHE_SSH
 docker volume rm DESKTOP_ENVIRONMENT_CACHE_VCSH_PRIVATE
