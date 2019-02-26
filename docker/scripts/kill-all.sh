7a3c1f22a9a1ec698d00c6cb62b5232062dde84e
Jackson Delahunt <jackson@stemn.com>
2019-02-26 15:08:18 +1100

    exclude

--- docker/scripts/kill-all.sh
+++ docker/scripts/kill-all.sh
@@ -6,5 +6,6 @@ export $($REPO_ROOT/docker/scripts/environment.sh)
 # Remove all non desktop environment containers
 docker ps -a | \
   grep -v " $DESKTOP_ENVIRONMENT_CONTAINER" | \
+  grep -v "CONTAINER " | \
   cut -b 1-20 | \
   xargs docker rm -f
