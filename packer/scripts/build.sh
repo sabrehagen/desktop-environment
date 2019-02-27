dae4c24352163cd8dea38503f681902b81ed79fc
Jackson Delahunt <jackson@stemn.com>
2019-02-28 10:17:52 +1100

    order

--- packer/scripts/build.sh
+++ packer/scripts/build.sh
@@ -3,11 +3,11 @@ REPO_ROOT=$(dirname $(readlink -f $0))/../..
 # Export desktop environment shell configuration
 export $($REPO_ROOT/docker/scripts/environment.sh)
 
-# Authenticate with cloud service provider
-$REPO_ROOT/credentials/authenticate.sh
-
 # Clone the desktop environment into a local docker volume
 $REPO_ROOT/docker/scripts/clone.sh
+
+# Authenticate with cloud service provider
+$REPO_ROOT/credentials/authenticate.sh
 
 docker run \
   --env GOOGLE_APPLICATION_CREDENTIALS=$DESKTOP_ENVIRONMENT_DOCKER_REPOSITORY/credentials/credentials.json \
