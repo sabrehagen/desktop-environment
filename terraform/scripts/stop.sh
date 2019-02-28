777457b2a8ea3ac45dc3d1ad100e386571403720
Jackson Delahunt <jackson@stemn.com>
2019-02-28 16:09:20 +1100

    working

--- terraform/scripts/stop.sh
+++ terraform/scripts/stop.sh
@@ -1,4 +1,5 @@
 REPO_ROOT=$(dirname $(readlink -f $0))/../..
 
 # Destroy the desktop environment cloud virtual machine
-$REPO_ROOT/terraform/scripts/terraform.sh destroy
+$REPO_ROOT/terraform/scripts/terraform.sh destroy \
+  -target google_compute_instance.desktop-environment
