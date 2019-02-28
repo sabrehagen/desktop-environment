9ca8c4c16c1113e8492e141191e7a04b1a1ccbf0
Jackson Delahunt <jackson@stemn.com>
2019-02-28 13:21:09 +1100

    args-working

--- terraform/scripts/terraform.sh
+++ terraform/scripts/terraform.sh
@@ -6,20 +6,24 @@ export $($REPO_ROOT/docker/scripts/environment.sh)
 # Authenticate with cloud service provider
 $REPO_ROOT/credentials/authenticate.sh
 
+# Capture command line arguments
+TERRAFORM_ARGS=$@
+
 # Execute the supplied terraform command
 docker run \
   --entrypoint ash \
   --env GOOGLE_APPLICATION_CREDENTIALS=$DESKTOP_ENVIRONMENT_DOCKER_REPOSITORY/credentials/credentials.json \
   --env TF_IN_AUTOMATION=true \
+  --env TF_INPUT=0 \
   --env TF_VAR_DESKTOP_ENVIRONMENT_CONTAINER=$DESKTOP_ENVIRONMENT_CONTAINER \
   --env TF_VAR_DESKTOP_ENVIRONMENT_REGISTRY=$DESKTOP_ENVIRONMENT_REGISTRY \
   --env TF_VAR_owner_host=$(hostname) \
   --env TF_VAR_owner_name=$(whoami) \
-  --interactive \
   --rm \
   --tty \
   --volume DESKTOP_ENVIRONMENT_DOCKER_REPOSITORY:$DESKTOP_ENVIRONMENT_DOCKER_REPOSITORY \
   --workdir $DESKTOP_ENVIRONMENT_DOCKER_REPOSITORY/terraform \
   hashicorp/terraform:light -c "\
+  echo XXXXXXX $TERRAFORM_ARGS;
   terraform init;
-  terraform $@"
+  terraform $TERRAFORM_ARGS"
