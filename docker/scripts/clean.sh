84f1536821df4a1980db3f7246ab395071e0a069
Jackson <jackson@stemn.com>
2019-03-03 02:49:31 +0000

    clean

--- docker/scripts/clean.sh
+++ docker/scripts/clean.sh
@@ -1,5 +1,5 @@
 REPO_ROOT=$(dirname $(readlink -f $0))/../..
 
-# Remove volumes that can be recreated from scratch, directly from the container filesystem
+# Remove stateful volumes that are recreated directly from the container filesystem
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_CACHE_VCSH
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_USER_HOME
