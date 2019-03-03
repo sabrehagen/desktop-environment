18362c7725d1a0c326438f6c68d378ac2a3b72b4
Jackson <jackson@stemn.com>
2019-03-03 10:57:21 +0000

    friends

--- docker/scripts/clean-secrets.sh
+++ docker/scripts/clean-secrets.sh
@@ -2,6 +2,7 @@ REPO_ROOT=$(dirname $(readlink -f $0))/../..
 
 # Remove volumes containing secrets
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_CACHE_SECRETS
+$REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_CACHE_SIGNAL
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_CACHE_SSH
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_CACHE_VCSH_PRIVATE
 
