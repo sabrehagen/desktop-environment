9840be178c798d622c84f6a9bab71242ef5ac3a8
Jackson <jackson@stemn.com>
2019-03-03 06:54:19 +0000

    wipe

--- docker/scripts/clean-hard.sh
+++ docker/scripts/clean-hard.sh
@@ -6,6 +6,8 @@ $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_CODE
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_GITHUB
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_JUMP
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_MUSIKCUBE
+$REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_SIGNAL
+$REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_TRAEFIK
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_ZOOM
 
 # Clean secrets and home
