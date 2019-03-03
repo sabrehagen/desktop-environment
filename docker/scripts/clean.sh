b22e465c7479193fcc2d10b58672a33b53cefe41
Jackson <jackson@stemn.com>
2019-03-03 02:41:42 +0000

    no-stop

--- docker/scripts/clean.sh
+++ docker/scripts/clean.sh
@@ -1,8 +1,5 @@
 REPO_ROOT=$(dirname $(readlink -f $0))/../..
 
-# Stop the running environment so there are no containers using the volumes, ignoring errors if it does not exist
-$REPO_ROOT/docker/scripts/stop.sh 2>/dev/null
-
 # Remove volumes that can be recreated from scratch, directly from the container filesystem
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_CACHE_VCSH
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_USER_HOME
