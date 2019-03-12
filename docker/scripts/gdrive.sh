3bef3addcabddd82016ba712280a24fe54c53060
Jackson <jackson@stemn.com>
2019-03-12 00:27:05 +0000

    final

--- docker/scripts/gdrive.sh
+++ docker/scripts/gdrive.sh
@@ -6,8 +6,6 @@ docker run \
   --name gdrive \
   --security-opt apparmor:unconfined \
   --volume /mnt/drive:/mnt/gdrive:shared \
-  --volume DESKTOP_ENVIRONMENT_CACHE_GDRIVE:~/.gdfuse \
-  --volume /mnt/gdrive:/mnt/gdrive:shared \
+  --volume DESKTOP_ENVIRONMENT_CACHE_GDRIVE:/config/.gdfuse \
+  --volume /mnt/drive:/mnt/gdrive:shared \
   mitcdh/google-drive-ocamlfuse
-
-# google-drive-ocamlfuse -headless -id 502305750839-286hf5btkfk90o1idtbqdm4dlab3d0gu.apps.googleusercontent.com -secret -o89GAAhKaCQnlQa0WWvjv5f
