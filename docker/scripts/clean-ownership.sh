824d9b5d8c47f11fbd6d646c04487927b477c250
Jackson Delahunt <jackson@stemn.com>
2019-04-17 12:05:01 +1000

    fixes

--- docker/scripts/clean-ownership.sh
+++ docker/scripts/clean-ownership.sh
@@ -3,7 +3,7 @@ REPO_ROOT=$(dirname $(readlink -f $0))/../..
 # Export desktop environment shell configuration
 eval "$($REPO_ROOT/docker/scripts/environment.sh)"
 
-SECRETS_VOLUMES=$(cat $REPO_ROOT/docker/scripts/clean*.sh | grep DESKTOP_ | grep -v SECRETS_ | cut -f 2 -d ' ' | sort | uniq)
+SECRETS_VOLUMES=$(cat $REPO_ROOT/docker/scripts/environment.sh | grep echo | grep DESKTOP_ | grep -v SECRETS_ | cut -b 13- | cut -f 1 -d = | sort | uniq)
 
 for VOLUME_NAME in $SECRETS_VOLUMES; do
 
