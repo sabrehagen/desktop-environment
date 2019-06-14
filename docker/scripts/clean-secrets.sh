59f331993d25f389475d9d68535cae9486be3998
Jackson Delahunt <sabrehagen@ozemail.com.au>
2019-06-14 11:23:32 +1000

    clean

--- docker/scripts/clean-secrets.sh
+++ docker/scripts/clean-secrets.sh
@@ -8,7 +8,6 @@ $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_CACHE_GDRIVE &
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_CACHE_SECRETS &
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_SIGNAL &
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_CACHE_SSH &
-$REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_CACHE_VCSH_PRIVATE &
 
 # Wait for cleans to complete in parallel
 wait
--- docker/scripts/clean.sh
+++ docker/scripts/clean.sh
@@ -4,7 +4,6 @@ REPO_ROOT=$(dirname $(readlink -f $0))/../..
 $REPO_ROOT/docker/scripts/stop.sh 2>/dev/null
 
 # Remove stateful volumes that are recreated directly from the container filesystem
-$REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_CACHE_VCSH &
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_USER_HOME &
 
 # Wait for cleans to complete in parallel
