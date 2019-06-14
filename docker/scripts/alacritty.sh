c7a0777de0745e6ffa419cb7a2669db7faf2b2cb
Jackson Delahunt <sabrehagen@ozemail.com.au>
2019-06-14 11:22:35 +1000

    simple

--- docker/scripts/alacritty.sh
+++ docker/scripts/alacritty.sh
@@ -4,4 +4,4 @@ REPO_ROOT=$(dirname $(readlink -f $0))/../..
 eval "$($REPO_ROOT/docker/scripts/environment.sh)"
 
 # Start a desktop environment detached from this shell
-nohup gosu $DESKTOP_ENVIRONMENT_USER alacritty </dev/null >/dev/null 2>&1 &
+nohup alacritty </dev/null >/dev/null 2>&1 &
