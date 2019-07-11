5a05a43749474248e1a28876c485245b92eda704
Jackson Delahunt <jackson@stemn.com>
2019-07-12 09:11:28 +1000

    tmux

--- docker/scripts/tmux.sh
+++ docker/scripts/tmux.sh
@@ -3,17 +3,10 @@ REPO_ROOT=$(dirname $(readlink -f $0))/../..
 # Export desktop environment shell configuration
 eval "$($REPO_ROOT/docker/scripts/environment.sh)"
 
-# Ensure the desktop environment shell session exists
-docker exec \
-  --detach \
-  $DESKTOP_ENVIRONMENT_CONTAINER_NAME \
-  tmux new-session \
-  -s desktop-environment-shell \
-  -d
-
 # Start a new tmux client attached to the desktop environment shell
 docker exec \
-  --detach \
+  --interactive \
+  --tty \
   $DESKTOP_ENVIRONMENT_CONTAINER_NAME \
   tmux new-session \
   -s desktop-environment-shell-$(date +%s) \
