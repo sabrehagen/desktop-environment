459c3bee5422e9b1ae49e98d6d4d7cf70bc78227
Jackson Delahunt <jackson@stemn.com>
2019-07-11 20:48:22 +1000

    silent

--- docker/scripts/alacritty.sh
+++ docker/scripts/alacritty.sh
@@ -4,4 +4,4 @@ REPO_ROOT=$(dirname $(readlink -f $0))/../..
 eval "$($REPO_ROOT/docker/scripts/environment.sh)"
 
 # Start a desktop environment detached from this shell
-docker exec $DESKTOP_ENVIRONMENT_CONTAINER_NAME alacritty &
+docker exec $DESKTOP_ENVIRONMENT_CONTAINER_NAME alacritty >/dev/null 2>&1 &
