2753eebcb02a17806d02a97d8bb8c4b1e4e1aba9
Jackson Delahunt <jackson@stemn.com>
2019-10-14 12:59:06 +1100

    jump

--- docker/scripts/recycle-dotfiles.sh
+++ docker/scripts/recycle-dotfiles.sh
@@ -7,6 +7,5 @@ $REPO_ROOT/docker/scripts/build-dotfiles.sh
 $REPO_ROOT/docker/scripts/pre-clean.sh
 
 # Reset the desktop environment state
-$REPO_ROOT/docker/scripts/clean.sh
 $REPO_ROOT/docker/scripts/start.sh
 $REPO_ROOT/docker/scripts/alacritty.sh
