ceac7a9f98f4065c95aa65c44aac19fdcb47141f
Jackson Delahunt <jackson@stemn.com>
2019-10-22 09:04:43 +1100

    neovim

--- docker/scripts/clean-hard.sh
+++ docker/scripts/clean-hard.sh
@@ -3,7 +3,6 @@ REPO_ROOT=$(dirname $(readlink -f $0))/../..
 # Remove volumes that can be recreated from scratch, but require manual configuration in app
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_CHROME &
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_CODE &
-$REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_GITHUB &
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_JUMP &
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_MUSIKCUBE &
 $REPO_ROOT/docker/scripts/wipe-volume.sh DESKTOP_ENVIRONMENT_STATE_TRAEFIK &
--- terraform/scripts/restart.sh
+++
@@ -1,5 +0,0 @@
-REPO_ROOT=$(dirname $(readlink -f $0))/../..
-
-# Authenticate with cloud service provider
-$REPO_ROOT/terraform/scripts/stop.sh
-$REPO_ROOT/terraform/scripts/start.sh
