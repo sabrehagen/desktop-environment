4e275d5de5f6c95296fb75666528f04f3ebe7cf0
Jackson Delahunt <jackson@stemn.com>
2020-01-20 21:10:26 +1100

    clean-and-fixes

---
+++ docker/scripts/bootstrap.sh
@@ -0,0 +1,10 @@
+REPO_ROOT=$(dirname $(readlink -f $0))/../..
+
+# Export desktop environment shell configuration
+eval "$($REPO_ROOT/docker/scripts/environment.sh)"
+
+# Prepare docker host volumes
+$REPO_ROOT/docker/scripts/take-ownership.sh
+
+# Start desktop environment
+$REPO_ROOT/docker/scripts/start.sh
