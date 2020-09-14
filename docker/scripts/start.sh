b3c23c6afc7d785e79582c1d9af3d27251083a57
Jackson Delahunt <jackson@stemn.com>
2020-09-14 19:01:01 +1000

    id

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -73,6 +73,8 @@ docker run \
 # Wait until the desktop environment container is running before proceeding
 until docker inspect $DESKTOP_ENVIRONMENT_CONTAINER_NAME | grep Status | grep -m 1 running >/dev/null; do sleep 1; done
 
+# Make the container docker group id the same as the host docker group id so mounted docker socket permissions match
+$REPO_ROOT/docker/scripts/exec-root.sh groupmod -g $(grep docker /etc/group | cut -f3 -d:) docker
 
 # Start the desktop environment inside the container
 $REPO_ROOT/docker/scripts/exec.sh /home/$DESKTOP_ENVIRONMENT_USER/.config/scripts/startup.sh
