282f9e9c5ca880727d92c18cd3e570bdb4da0261
Jackson Delahunt <jackson@stemn.com>
2021-01-06 18:53:27 +1100

    final

--- docker/scripts/push.sh
+++ docker/scripts/push.sh
@@ -1,6 +1,7 @@
 REPO_ROOT=$(dirname $(readlink -f $0))/../..
+IMAGE=${1-base}
 
 # Export desktop environment shell configuration
 eval "$($REPO_ROOT/docker/scripts/environment.sh)"
 
-docker push $DESKTOP_ENVIRONMENT_REGISTRY/$DESKTOP_ENVIRONMENT_CONTAINER_IMAGE:$DESKTOP_ENVIRONMENT_CONTAINER_TAG
+docker push $DESKTOP_ENVIRONMENT_REGISTRY/$DESKTOP_ENVIRONMENT_CONTAINER_IMAGE-$IMAGE:$DESKTOP_ENVIRONMENT_CONTAINER_TAG
