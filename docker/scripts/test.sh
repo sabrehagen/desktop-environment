2530f9f52a589d85e66a8f2c4ef266a8ee7124a7
Jackson Delahunt <jackson@stemn.com>
2023-08-24 19:41:47 +1000

    headless

---
+++ docker/scripts/test.sh
@@ -0,0 +1,30 @@
+REPO_ROOT=$(dirname $(readlink -f $0))/../..
+
+# Export desktop environment shell configuration
+eval "$($REPO_ROOT/docker/scripts/environment.sh)"
+
+# Ensure the desktop environment test network exists
+docker network create $DESKTOP_ENVIRONMENT_DOCKER_NETWORK
+
+# Set the desktop environment test container name
+DESKTOP_ENVIRONMENT_CONTAINER_NAME=${DESKTOP_ENVIRONMENT_CONTAINER_IMAGE}-test-$(date +%s)
+
+# Start the desktop environment test container
+docker run \
+  --detach \
+  --name $DESKTOP_ENVIRONMENT_CONTAINER_NAME \
+  --rm \
+  $DESKTOP_ENVIRONMENT_REGISTRY/$DESKTOP_ENVIRONMENT_CONTAINER_IMAGE \
+  sleep infinity
+
+# Wait until the desktop environment test container is running before proceeding
+timeout 10 sh -c "until docker inspect $DESKTOP_ENVIRONMENT_CONTAINER_NAME | grep Status | grep -m 1 running >/dev/null; do sleep 1; done"
+
+# Check desktop environment container started successfully
+TEST_RESULT=$?
+
+# Remove desktop environment test container
+docker rm -f $DESKTOP_ENVIRONMENT_CONTAINER_NAME
+
+# Exit with test result exit code
+exit $TEST_RESULT
