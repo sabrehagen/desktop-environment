0b37f0be136fcfe88aa5f9f08e58b76c8cfd746b
Jackson Delahunt <jackson@stemn.com>
2023-08-24 19:49:08 +1000

    fix-env

--- docker/scripts/headless.sh
+++ docker/scripts/headless.sh
@@ -6,19 +6,16 @@ eval "$($REPO_ROOT/docker/scripts/environment.sh)"
 # Ensure the desktop environment test network exists
 docker network create $DESKTOP_ENVIRONMENT_DOCKER_NETWORK
 
-# Set the desktop environment test container name
-DESKTOP_ENVIRONMENT_CONTAINER_NAME=${DESKTOP_ENVIRONMENT_CONTAINER_IMAGE}-headless
-
 # Start the desktop environment test container
 docker run \
   --detach \
-  --name $DESKTOP_ENVIRONMENT_CONTAINER_NAME \
+  --name $DESKTOP_ENVIRONMENT_CONTAINER_IMAGE \
   --rm \
   $DESKTOP_ENVIRONMENT_REGISTRY/$DESKTOP_ENVIRONMENT_CONTAINER_IMAGE \
   sleep infinity
 
 # Wait until the desktop environment test container is running before proceeding
-timeout 10 sh -c "until docker inspect $DESKTOP_ENVIRONMENT_CONTAINER_NAME | grep Status | grep -m 1 running >/dev/null; do sleep 1; done"
+timeout 10 sh -c "until docker inspect $DESKTOP_ENVIRONMENT_CONTAINER_IMAGE | grep Status | grep -m 1 running >/dev/null; do sleep 1; done"
 
 # Start the desktop environment inside the container
 $REPO_ROOT/docker/scripts/exec.sh /home/$DESKTOP_ENVIRONMENT_USER/.config/scripts/startup.sh
