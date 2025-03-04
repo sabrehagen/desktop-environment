4a72dacabab9193b3250f83a426693d5db8e821c
Jackson Delahunt <jackson@stemn.com>
2025-03-04 17:58:07 +1100

    cachebust-apps

--- docker/scripts/build-apps.sh
+++ docker/scripts/build-apps.sh
@@ -3,8 +3,8 @@ REPO_ROOT=$(dirname $(readlink -f $0))/../..
 # Export desktop environment shell configuration
 eval "$($REPO_ROOT/docker/scripts/environment.sh)"
 
-# Set the cachebust file to rebuild from dotfiles onwards
-CACHEBUST_DOTFILES=$(date +%s > $REPO_ROOT/.cachebust-dotfiles)
+# Set the cachebust file to rebuild from apps onwards
+CACHEBUST_APPS=$(date +%s > $REPO_ROOT/.cachebust-apps)
 
 # Rebuild the desktop environment
 $REPO_ROOT/docker/scripts/build.sh
