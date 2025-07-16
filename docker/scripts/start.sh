87d7912095b505e4a206afe06586ef4bf49a548d
Jackson Delahunt <jackson@stemn.com>
2025-07-16 23:05:47 +1000

    no-etc-hosts

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -43,7 +43,6 @@ docker run \
   --publish 8822:22 \
   --rm \
   --security-opt apparmor:unconfined \
-  --volume /etc/hosts:/etc/hosts \
   --volume /dev/displaylink:/dev/displaylink \
   --volume /dev/shm:/dev/shm \
   --volume /mnt/mmc:/mnt/mmc \
