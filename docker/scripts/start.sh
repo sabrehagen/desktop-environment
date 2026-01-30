ccd792dbe32b8f838b609f6716e019cf902e953a
Jackson Delahunt <jackson@stemn.com>
2026-01-30 15:48:51 +1100

    no-host-dbus

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -50,7 +50,6 @@ docker run \
   --volume /dev/displaylink:/dev/displaylink \
   --volume /dev/shm:/dev/shm \
   --volume /mnt/mmc:/mnt/mmc \
-  --volume /run/dbus/system_bus_socket:/run/dbus/system_bus_socket \
   --volume /run/udev:/run/udev \
   --volume /var/lib/docker:/var/lib/docker \
   --volume /var/run/docker.sock:/var/run/docker.sock \
