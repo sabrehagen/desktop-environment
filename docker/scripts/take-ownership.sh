6ccca77d05a6fc9b87b4b7dc0f4675a97baec152
Jackson Delahunt <jackson@stemn.com>
2026-02-02 00:45:21 +1100

    fix-take-ownership

--- docker/scripts/take-ownership.sh
+++ docker/scripts/take-ownership.sh
@@ -3,25 +3,28 @@ REPO_ROOT=$(dirname $(readlink -f $0))/../..
 # Export desktop environment shell configuration
 eval "$($REPO_ROOT/docker/scripts/environment.sh)"
 
-DESKTOP_ENVIRONMENT_VOLUMES=$(cat $REPO_ROOT/docker/scripts/environment.sh | grep echo | grep -E 'CACHE|STATE|USER_' | grep -v '_ID' | cut -d' ' -f3 | cut -d= -f1)
+DESKTOP_ENVIRONMENT_VOLUMES=$(cat $REPO_ROOT/docker/scripts/environment.sh | grep echo | grep -E 'CACHE|STATE|USER_' | grep -vE '_ID|X11' | cut -d' ' -f3 | cut -d= -f1)
 
 for VOLUME_NAME in $DESKTOP_ENVIRONMENT_VOLUMES; do
 
   VOLUME_PATH=$(printenv $VOLUME_NAME)
 
-  # If the volume path is non-writable
-  if [ ! -w "$VOLUME_PATH" ]; then
-    echo Owning $VOLUME_NAME...
-
-    # Give the desktop environment user ownership of the volume
-    docker run \
-      --quiet \
-      --rm \
-      --user root \
-      --volume $VOLUME_NAME:$VOLUME_PATH \
-      alpine:latest \
-      chown -R $DESKTOP_ENVIRONMENT_USER_ID:$DESKTOP_ENVIRONMENT_USER_ID $VOLUME_PATH &
-  fi
+  # Take ownership if the volume is owned by root
+  docker run \
+    --env DESKTOP_ENVIRONMENT_USER_ID=$DESKTOP_ENVIRONMENT_USER_ID \
+    --env VOLUME_NAME=$VOLUME_NAME \
+    --env VOLUME_PATH=$VOLUME_PATH \
+    --quiet \
+    --rm \
+    --user root \
+    --volume $VOLUME_NAME:$VOLUME_PATH \
+    alpine:latest \
+    sh -c '
+      if [ "$(stat -c %U "$VOLUME_PATH")" = "root" ]; then
+        echo Owning $VOLUME_NAME...
+        chown -R $DESKTOP_ENVIRONMENT_USER_ID:$DESKTOP_ENVIRONMENT_USER_ID "$VOLUME_PATH"
+      fi
+    ' &
 
 done
 
