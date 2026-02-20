08ffa4598c35b7fb2a50b407ff3784c3724b900e
Jackson Delahunt <jackson@stemn.com>
2026-02-20 16:59:54 +1100

    no-pre-clean

--- docker/scripts/recycle-dotfiles.sh
+++ docker/scripts/recycle-dotfiles.sh
@@ -3,8 +3,5 @@ REPO_ROOT=$(dirname $(readlink -f $0))/../..
 # Rebuild the desktop environment
 $REPO_ROOT/docker/scripts/build-dotfiles.sh
 
-# Check for data loss before recycling
-$REPO_ROOT/docker/scripts/pre-clean.sh
-
 # Reset the desktop environment state
 $REPO_ROOT/docker/scripts/restart.sh
