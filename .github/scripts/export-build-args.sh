ae76265325f4588f8727a9c635861f27f862d268
Jackson Delahunt <jackson@stemn.com>
2026-03-20 17:06:15 +1100

    workflow-buildargs

---
+++ .github/scripts/export-build-args.sh
@@ -0,0 +1,2 @@
+BUILD_ARGS=$(docker/scripts/environment.sh | grep -E '^export DESKTOP_ENVIRONMENT_(USER|GITHUB_USER|SOURCE_)' | sed 's/^export //')
+printf 'build_args<<EOF\n%s\nEOF\n' "$BUILD_ARGS" >> $GITHUB_OUTPUT
