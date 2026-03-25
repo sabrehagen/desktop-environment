18a78d02f34213004dcde6c87653db36a1e839d2
Jackson Delahunt <jackson@stemn.com>
2026-03-25 14:46:31 +1100

    update-parallel

--- docker/scripts/sources-update.sh
+++ docker/scripts/sources-update.sh
@@ -1,4 +1,5 @@
 # Update all desktop environment source repositories
 for repo in $DESKTOP_ENVIRONMENT_SOURCE_HOME/*/; do
-  test -d $repo/.git && git -C $repo pull --ff-only 2>&1 || true
+  { test -d $repo/.git && git -C $repo pull --ff-only 2>&1 || true; } &
 done
+wait
