d782f0ad86aa666b0b50b7b467b75be86ba43b61
Jackson Delahunt <jackson@stemn.com>
2026-04-18 07:12:28 +1000

    no-exec-root

--- docker/scripts/exec-root.sh
+++
@@ -1,9 +0,0 @@
-REPO_ROOT=$(dirname $(readlink -f $0))/../..
-
-# Export desktop environment shell configuration
-eval "$($REPO_ROOT/docker/scripts/environment.sh)"
-
-docker exec \
-  --interactive \
-  --user root \
-  $DESKTOP_ENVIRONMENT_CONTAINER_IMAGE zsh -c "$*"
